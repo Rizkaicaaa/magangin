e04b0e2c7d68e3b39a8dfbd18f6fbf3b5b7b79bc
Rizka Kurnia Illahi <rizkakurniaillahi@gmail.com>
2025-09-21 21:14:51 +0700

    Update routes di branch rizka

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,11 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\JadwalKegiatanController;
+use App\Http\Controllers\InfoOrController; 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HasilWawancaraController;
+use App\Http\Controllers\JadwalSeleksiController;
+use App\Http\Controllers\PendaftarController;;
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,6 +20,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/kelola-info-or', [InfoOrController::class, 'index'])->name('info-or.index');
+    Route::resource('hasilwawancara', HasilWawancaraController::class);
+
+    Route::resource('/jadwal-seleksi', JadwalSeleksiController::class);
 });
 
 // Route::get('/auth', function () {
@@ -26,10 +35,16 @@ Route::get('/penilaian', function () {
     return view('penilaian.index');
 });
 
-Route::get('/info-or', function () {
-    return view('info_or.index');
+Route::get('/pendaftar', function () {
+    return view('pendaftar.index');
 });
 
+Route::get('/info-or', [InfoOrController::class, 'index'])->name('info-or.index');
+Route::post('/kelola-info-or', [InfoOrController::class, 'store'])->name('info-or.store');
+Route::put('/kelola-info-or/{id}/tutup', [InfoOrController::class, 'updateStatus'])->name('info-or.tutup');
+
+
+// Routes untuk Jadwal Kegiatan Management
 
 // Routes untuk Jadwal Kegiatan Management
 Route::middleware(['auth'])->group(function () {
@@ -41,6 +56,7 @@ Route::middleware(['auth'])->group(function () {
     Route::prefix('jadwal-kegiatan')->name('jadwal-kegiatan.')->group(function () {
         
         // Get kegiatan by periode (untuk load tabel)
+        Route::get('/api/by-periode', [JadwalKegiatanController::class, 'getByPeriode'])->name('by-periode');
         
         // CRUD operations
         Route::post('/', [JadwalKegiatanController::class, 'store'])->name('store');
@@ -50,6 +66,17 @@ Route::middleware(['auth'])->group(function () {
         
     });
     
+});
+
+
+Route::middleware(['auth'])->group(function () {
+    // Routes untuk mengelola data pendaftar
+    Route::get('/pendaftar', [PendaftarController::class, 'index'])->name('pendaftar.index');
+    Route::get('/pendaftar/{id}', [PendaftarController::class, 'show'])->name('pendaftar.show');
+    Route::put('/pendaftar/{id}/status', [PendaftarController::class, 'updateStatus'])->name('pendaftar.update-status');
+    Route::post('/pendaftar/{id}/dinas', [PendaftarController::class, 'setDinasDiterima'])->name('pendaftar.set-dinas');
+    Route::get('/pendaftar/{id}/download-cv', [PendaftarController::class, 'downloadCV'])->name('pendaftar.download-cv');
+    Route::get('/pendaftar/{id}/download-transkrip', [PendaftarController::class, 'downloadTranskrip'])->name('pendaftar.download-transkrip');
 });
 
 require __DIR__.'/auth.php';
