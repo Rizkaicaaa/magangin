661d1871409ee39254d10fcb2ddb9b8da08551c2
thurrr1 <rahmanthur1@gmail.com>
2025-12-08 17:14:10 +0700

    test tanpa eror tapi bukan yang gabungan

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,10 +1,19 @@
 <?php
 
 namespace Tests;
+use Mockery;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     //
+}
+
+protected function tearDown(): void
+{
+    if (class_exists(Mockery::class)) {
+        Mockery::close();
+    }
+    parent::tearDown();
 }
